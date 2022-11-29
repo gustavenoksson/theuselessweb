951822ea4750c6d72361aef6887358e5f8d9dbef
gustavenoksson <gustav.enoksson@live.se>
2022-11-29 09:54:51 +0100

    added fonts

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const body = document.querySelector('body');
-const main = document.querySelector('main');
+// const body = document.querySelector('body');
+// const main = document.querySelector('main');
 
-const getFact = async () => {
-  const res = await fetch(
-    'https://uselessfacts.jsph.pl/random.json?language=en'
-  );
-  const facts = await res.json();
+// const getFact = async () => {
+//   const res = await fetch(
+//     'https://uselessfacts.jsph.pl/random.json?language=en'
+//   );
+//   const facts = await res.json();
 
-  console.log(facts.text);
-};
+//   console.log(facts.text);
+// };
 
 // fetch('https://uselessfacts.jsph.pl/random.json?language=en')
 //   .then((response) => response.json())
@@ -16,4 +17,9 @@ const getFact = async () => {
 //     console.log(fact.text);
 //   });
 
-getFact();
+// getFact();
+
+const insperationalWords = [
+  'Think of the money you are making!',
+  'The payraise is coming!',
+];

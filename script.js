38f35a2a5cbf3b6de67a33d09467d02c6fbb6de6
gustavenoksson <gustav.enoksson@live.se>
2022-11-29 12:02:58 +0100

    added screensaver

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const body = document.querySelector('body');
 
-const powerBtn = document.querySelector('.powerBtn');
 const screen = document.querySelector('.screen');
-
-const welcomeText = 'Welcome to work!';
+const powerBtn = document.querySelector('.powerBtn');
+const screenSaver = document.querySelector('.powerSaver');
 
 const insperationalWords = [
   'Think of the money you are making!',
@@ -12,6 +11,9 @@ const insperationalWords = [
 
 const keyboardKey = [];
 
-powerBtn.addEventListener('click', () => {
-  screen.style.backgroundColor = 'green';
-});
+const computerStart = () => {
+  powerBtn.style.backgroundColor = 'green';
+  screen.classList.add('screenStart');
+};
+
+powerBtn.addEventListener('click', computerStart);

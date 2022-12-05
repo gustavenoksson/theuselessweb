ca6706e4b068ce05bb89a2f179f77b54b5eab12c
Gustav Enoksson <gustav.enoksson@live.se>
2022-12-05 15:20:32 +0100

    100% width & height on background img

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const body = document.querySelector('body');
+const main = document.querySelector('main');
 
+const keyboard = document.querySelector('.keyboard');
+const chassis = document.querySelector('.chassis');
 const screen = document.querySelector('.screen');
 const powerBtn = document.querySelector('.powerBtn');
 const screenSaver = document.querySelector('.powerSaver');
-const computerBackground = document.querySelector('.screenBackground');
 
 const insperationalWords = [
   'Think of the money you are making!',
@@ -14,8 +16,17 @@ const keyboardKey = [];
 
 const computerStart = () => {
   powerBtn.style.backgroundColor = 'green';
-  computerBackground.classList.toggle('hidden');
+
+  const computerBackground = document.createElement('img');
+  computerBackground.src = 'images/windowsBackground.jpeg';
+  chassis.appendChild(computerBackground);
+  computerBackground.classList.add('screenBackground');
   computerBackground.classList.add('loadIn');
+
+  chassis.classList.add('chassisFullSize');
+
+  screen.classList.add('hidden');
+  keyboard.classList.add('hidden');
 };
 
 powerBtn.addEventListener('click', computerStart);

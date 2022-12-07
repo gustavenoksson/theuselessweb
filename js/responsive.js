1209a1783fdfc6a58682ddf47d8147bde6446f31
gustavenoksson <gustav.enoksson@live.se>
2022-12-07 13:42:33 +0100

    fixed display hidden on keyboard

--- js/responsive.js
+++ js/responsive.js
@@ -1,14 +1,14 @@
-const desktopWidth = window.matchMedia('(min-width: 1024px)');
+// const desktopWidth = window.matchMedia('(min-width: 1024px)');
 
-const windowResize = (width) => {
-  if (width.matches) {
-    startBtn.addEventListener('click', () => {
-      startDevice('url(/images/windowsBackground.jpeg)');
-    });
-  } else {
-    startBtn.addEventListener('click', () => {
-      startDevice('url(/images/mobileBackground.jpeg)');
-    });
-  }
-};
-windowResize(desktopWidth);
+// const windowResize = (width) => {
+//   if (width.matches) {
+//     startBtn.addEventListener('click', () => {
+//       startDevice('url(/images/windowsBackground.jpeg)');
+//     });
+//   } else {
+//     startBtn.addEventListener('click', () => {
+//       startDevice('url(/images/mobileBackground.jpeg)');
+//     });
+//   }
+// };
+// windowResize(desktopWidth);
--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const main = document.querySelector('main');
 const device = document.querySelector('.device');
 const screen = document.querySelector('.screen');
 const startBtn = document.querySelector('.startBtn');
+const keyboard = document.querySelector('.keyboard');
 const btnSquare = document.querySelector('.btnSquare');
 
 const insperationalWords = [
@@ -10,7 +11,10 @@ const insperationalWords = [
   'The payraise is coming I swear!',
 ];
 
-const startDevice = (url) => {
-  screen.style.backgroundImage = url;
-  device.classList.add('deviceFullSize');
+const startDevice = () => {
+  screen.classList.toggle('backgroundImg');
+  device.classList.toggle('deviceFullSize');
+  keyboard.style.display = 'none';
 };
+
+startBtn.addEventListener('click', startDevice);
